29a9e45e607a32129bd72ef31d4d25d3e5fad2d0
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-02-15 15:51:12 +0500

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import Dashboard from "./dashboard/page";
-import { AuthProvider } from "@/app/context/AuthProvider";
+
 export default function Home() {
   return (
     <main className=" h-full ">
-      <AuthProvider>
-        <Dashboard />
-      </AuthProvider>
+      <Dashboard />
     </main>
   );
 }
